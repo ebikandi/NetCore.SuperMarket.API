348915bdbfddcfa4eb1ea0cca319811c0dbfd59d
Eneko Bikandi <ebikandi001@gmail.com>
2019-02-27 22:29:10 +0100

    introducing controllers

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.SuperMarket.API.Controllers
+{
+    [Route("/api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        
+    }
+}
